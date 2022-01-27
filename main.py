221186eccdf4e874d076eb9349ec9d3834b9de02
Kamrul Hasan <hasankamrul2097@gmail.com>
2022-01-28 00:50:12 +0600

    Fitlering Empty Element in Dictionary

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from src.HTML_Parsing import *
 from src.PDF_Parsing import *
 from src.Download_PDF import *
 
+
 def main():
     html_links = Generating_URL(show=False)
     pdf_links = Parsing_HTML_For_PDF_Links(html_links)
     Downloading_PDF(html_links, pdf_links)
-    db = Parsing_PDF()
-    print(db)
-
+    students = Parsing_PDF()
+    #Filters out empty ones
+    students = [student for student in students if list(student.values()) != [[]]]
+    print(len(students))
 if __name__ == "__main__":
     main()
