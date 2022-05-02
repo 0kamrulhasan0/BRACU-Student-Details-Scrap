99ddc9783879af03b93f0a1a6128b8dca1ccd71a
Kamrul Hasan <hasankamrul2097@gmail.com>
2022-05-03 02:33:27 +0600

    removed old files

--- src/CONSTANTS.py
+++
@@ -1,6 +0,0 @@
-ROOT_DIR = 'PDF_Archieve'
-PICKLE_DIR = 'Pickle_Archieve'
-SEAT_PLAN_HTML_LINKS = f'{PICKLE_DIR}/Seat_Plan_HTML_Links.pkl'
-PDF_LINKS = f'{PICKLE_DIR}/PDF_Links.pkl'
-STUDENT_INFO = f'{PICKLE_DIR}/Student_Info_Unstructured.pkl'
-CERT = False
